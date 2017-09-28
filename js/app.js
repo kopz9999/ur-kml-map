47f2ffdc0800d63fa50ac4c3c9c46ff175e94468
Luis Canales <kopz9999@gmail.com>
2017-09-27 23:53:34 -0700

    fix icons on map

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ var infowindow;
 
 //A utility function that translates a given style to an icon
 function getIcon(style) {
+  console.log(style);
   switch (style) {
     case "#icon-1753-0288D1":
       return "img/green-atm.png";
@@ -123,19 +124,33 @@ function doFilter() {
 }
 
 $(function() {
+  var accordionContainer = jQuery('.accordion-mode');
+  var searchMode = jQuery(".search-mode");
 
-//toggle side nav
-  $(".sidenav-toggle").click(function() {
-    $(".uofrmap-sidenav").toggleClass("active");
-  });
+  //toggle side nav
+  function initToggleNav() {
+    var sidebar = jQuery(".uofrmap-sidenav");
 
-//initialise a map
+    jQuery(".sidenav-toggle").click(function() {
+      sidebar.toggleClass("active");
+      searchMode.addClass('hidden');
+      accordionContainer.removeClass('hidden');
+    });
+    jQuery(".sidenav-search-toggle").click(function() {
+      sidebar.toggleClass("active");
+      searchMode.removeClass('hidden');
+      accordionContainer.addClass('hidden');
+      searchMode.find('.mobile-search-bar').focus();
+    });
+  }
 
+  initToggleNav();
+  //initialise a map
   init();
 
-// pull from locally hosted copy for access to data
+  // pull from locally hosted copy for access to data
 
-  $.get("map/map.kml", function(data) {
+  jQuery.get("map/map.kml", function(data) {
 
     html = "";
     showall = "";
@@ -155,12 +170,11 @@ $(function() {
 
       if (index === 0 || $(this).find("name").eq(0).text() !== comparefolder) {
         accordion = "<div class=\"accordion " + convertToSlug(folder) + "\"><input value=\"" + convertToSlug(folder) + "\"  id=\"" + convertToSlug(folder) + "\" type=\"checkbox\"  name=\"accordions\"><label for=\"" + convertToSlug(folder) + "\">" + folder + "</label><div class=\"accordion-content\"><ul></ul></div></div>";
-        $(".uofrmap-sidenav").append(accordion);
+        accordionContainer.append(accordion);
       }
 //use to compare if first instance
       comparefolder = folder;
-
-    })
+    });
     mymarkers = "";
 
     var length = $(data).find("Placemark").length;
@@ -191,10 +205,10 @@ $(function() {
         "style": style.trim()
       })
 //output as a navigation
-      html = "<li class=\"" + convertToSlug(style) + "\"><img src=\"" + getIcon(style.type) + "\" alt=\"" + place + "\">" + place + "</li>";
+      html = "<li class=\"" + convertToSlug(style) + "\"><img src=\"" + getIcon(style) + "\" alt=\"" + place + "\">" + place + "</li>";
 
 
-      showall += "<li class=\"" + convertToSlug(style) + "\"><img src=\"" + getIcon(style.type) + "\" alt=\"" + place + "\">" + place + "</li>";
+      showall += "<li class=\"" + convertToSlug(style) + "\"><img src=\"" + getIcon(style) + "\" alt=\"" + place + "\">" + place + "</li>";
 
 
 //add places to category
@@ -305,83 +319,6 @@ $(function() {
       setUpSearchBar(jQuery, map, markers);
       /* */
     }, 200);
-
-//A utility function that translates a given style to an icon
-    function getIcon(style) {
-      switch (style) {
-        case "#icon-1753-0288D1":
-          return "img/green-atm.png";
-        case "baby":
-          return "img/ed-baby.png";
-        case "#icon-1753-0288D1":
-          return "img/green-atm.png";
-        case "barbell":
-          return "img/blue-barbell.png";
-        case "#icon-1519-0288D1":
-          return "img/blue-sports.png";
-        case "bed":
-          return "img/red-bed.png";
-        case "#icon-1522-0288D1":
-          return "img/purple-bike.png";
-        case "bike":
-          return "img/blue-bike.png";
-        case "#icon-1664-0288d1":
-          return "img/lightpurple-book.png";
-        case "#icon-1899-DB4436":
-          return "img/red-book.png";
-        case "#icon-1546-DB4436":
-          return "img/red-buildings.png";
-        // case "clipboard":
-        // return "img/clipboard.png";
-        case "coffee":
-          return "img/orange-coffee.png";
-        case "#icon-1820-0288D1":
-          return "img/green-laptop.png";
-        case "#icon-1547-0288D1-nodesc":
-          return "img/purple-downtown.png";
-        case "#icon-1577-0288D1":
-          return "img/orange-fastfood.png";
-        case "#icon-1577-DB4436":
-          return "img/orange-food.png";
-        case "#icon-1548-0288D1":
-          return "img/purple-pillars.png";
-        case "#icon-1646-DB4436":
-          return "img/green-health.png";
-        case "microscope":
-          return "img/purple-science.png";
-        //case "minus": return "img/minus.png";
-        case "#icon-1548-0288D1":
-          return "img/red-music.png";
-        case "paint-blue":
-          return "img/blue-paint.png";
-        case "#icon-1509-0288D1":
-          return "img/purple-paint.png";
-        case "rcmp":
-          return "img/purple-rcmp.png";
-        case "running":
-          return "img/blue-running.png";
-        case "#icon-1685-0288D1":
-          return "img/shop-green.png";
-        case "shop-orange":
-          return "img/orange-retail.png";
-        case "#icon-1680-0288D1":
-          return "img/blue-sports.png";
-        case "#icon-1701-0288D1":
-          return "img/blue-swim.png";
-        case "tennis":
-          return "img/blue-tennis.png";
-        case "#icon-1709-0288D1":
-          return "img/blue-threatre.png";
-        case "ticket":
-          return "img/purple-ticket.png";
-        case "#icon-1886-0288D1":
-          return "img/blue-tree.png";
-        case "#icon-1890-0288D1":
-          return "img/blue-volley.png";
-        default:
-          return "img/blue-dot.png";
-      }
-    }
   });
 
   if (!Array.prototype.forEach) {
